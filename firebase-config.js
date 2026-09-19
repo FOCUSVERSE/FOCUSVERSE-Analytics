9b6ee7983ee6277f38d8ac1e83364afb797dc26a
FOCUSVERSE <panditji11029@gmail.com>
2026-09-19 22:50:53 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.firebasestorage.app",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME"
+  apiKey: "AIzaSyAg1kskficoxaoZ7KDVDT9_DgVMTIbQdo",
+  authDomain: "focusverse-analytics.firebaseapp.com",
+  projectId: "focusverse-analytics",
+  storageBucket: "focusverse-analytics.firebasestorage.app",
+  messagingSenderId: "193594300797",
+  appId: "1:193594300797:web:7e750d2216766d11ea1414",
+  measurementId: "G-QBH73WHHFC"
 };
-export const ADMIN_EMAIL = "YOUR_ADMIN_EMAIL@example.com";
+
+export const ADMIN_EMAIL = "panditji11029@gmail.com";
