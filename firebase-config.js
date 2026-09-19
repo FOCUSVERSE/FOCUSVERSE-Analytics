81571bbd33fb90e423b0cfed760dbd4ca5503608
FOCUSVERSE <panditji11029@gmail.com>
2026-09-19 22:50:26 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME.firebaseapp.com",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME.firebasestorage.app",
+  messagingSenderId: "REPLACE_ME",
+  appId: "REPLACE_ME"
+};
+export const ADMIN_EMAIL = "YOUR_ADMIN_EMAIL@example.com";
